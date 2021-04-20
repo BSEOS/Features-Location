e4ca2a4c6a92921ca44ecd9bd4a5ce406305ef08
Lounes Brahimi <lounesnewgs5@gmail.com>
2021-04-20 06:57:25 +0200

    remove superflue comments

--- LSA/algorithm.ts
+++ LSA/algorithm.ts
@@ -265,20 +265,6 @@ class Lsa{
 
 var fs = require("fs");
 
-//let documents : String[] = [ 
-//  "The Neatest Little Guide to Stock Market Investing", 
-//  "Investing For Dummies, 4th Edition", 
-//  "The Little Book of Common Sense Investing: The Only Way to Guarantee Your Fair Share of Stock Market Returns",
-//  "The Little Book of Value Investing", 
-//  "Value Investing: From Graham to Buffett and Beyond", 
-//  "Rich Dad's Guide to Investing: What the Rich Invest in, That the Poor and the Middle Class Do Not!", 
-//  "Investing in Real Estate, 5th Edition", 
-//  "Stock Investing For Dummies", 
-//  "Rich Dad's Advisors: The ABC's of Real Estate Investing: The Secrets of Finding Hidden Profits Most Investors Miss" 
-//];
-
-//let stopwords : String[] = ["and","edition","for","in","little","of","the","to", "", ''];
-
 let docs = new Lsa();
 
 docs.readDocument('./../Samples/document1.txt');
