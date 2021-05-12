8f50d70f22e3d59a59fbb7b1bb08e668a94ea584
edwinans <edyansari5@gmail.com>
2021-05-12 14:33:46 +0200

    TreeView by filtered docs

--- src/extension.ts
+++ src/extension.ts
@@ -1,58 +1,69 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 import { Uri, Range, Position } from 'vscode';
 
-import { LSA } from './algorithm'
+import { LSA } from './algorithm';
+import { FeaturesLocator } from './featuresView';
 
-
+import { FileExplorer } from './fileExplorer';
+import { DepNodeProvider, Dependency } from './nodeDependencies';
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 
 
-
 export async function activate(context: vscode.ExtensionContext) {
+    let docs = ['/home/edwin/Desktop/Cours/S2/PSTL/BankWebWithVariability/backend/src/models/Account.cs', '/home/edwin/Desktop/Cours/S2/PSTL/BankWebWithVariability/backend/src/models/Bank.cs', '/home/edwin/Desktop/Cours/S2/PSTL/BankWebWithVari…ity/backend/src/Controllers/AccountController.cs', '/home/edwin/Desktop/Cours/S2/PSTL/BankWebWithVariability/backend/src/models/Consortium.cs', '/home/edwin/Desktop/Cours/S2/PSTL/BankWebWithVariability/backend/src/models/Converter.cs', '/home/edwin/Desktop/Cours/S2/PSTL/BankWebWithVariability/backend/src/Program.cs', '/home/edwin/Desktop/Cours/S2/PSTL/BankWebWithVariability/backend/src/Startup.cs']
+    let curPath = "";
 
-    // algo.f()
-    let lsa_obj = new LSA()
-    let req = "limit"
-    let dir = "C:/Users/BUYMORE/Pictures/sorbonne/PSTL/BankWebWithVariability/backend/src"
-    let stop_file = "C:/Users/BUYMORE/Pictures/sorbonne/PSTL/git2/Features-Location/Samples/stopwords.json"
-    let res = await lsa_obj.lsa(req, dir, stop_file)
-    console.log(lsa_obj.documents_name)
+    if (vscode.workspace.workspaceFolders) {
+        curPath = vscode.workspace.workspaceFolders[0].uri.path;
+    }
 
-    // let map: Map<String, Range[]> = new Map<String, Range[]>();
-    // map.set("file", [new Range(new Position(2, 3), new Position(4, 6))]);
-    // console.log(map.get("file"))
+    // let lsa_obj = new LSA()
+    // let req = "limit"
+    // let dir = curPath
+    // let stop_file = "/home/edwin/Desktop/Cours/S2/PSTL/features-location/Samples/stopwords.json"
+    // let res = await lsa_obj.lsa(req, dir, stop_file)
+    // console.log(lsa_obj.documents_name)
 
-    // Use the console to output diagnostic information (console.log) and errors (console.error)
-    // This line of code will only be executed once when your extension is activated
+
+
+    new FeaturesLocator(context, docs);
+
+    vscode.window.registerTreeDataProvider(
+        'nodeDependencies',
+        new DepNodeProvider(curPath)
+    );
+
+    let map: Map<String, Range[]> = new Map<String, Range[]>();
+    map.set("file", [new Range(new Position(2, 3), new Position(4, 6))]);
+    console.log(map.forEach((value, key) => { console.log(value) }))
+
     console.log('Congratulations, your extension "features-location" is now active!');
 
-    // The command has been defined in the package.json file
-    // Now provide the implementation of the command with registerCommand
-    // The commandId parameter must match the command field in package.json
-    let disposable = vscode.commands.registerCommand('features-location.helloWorld', async (fileUri) => {
+
+    let disposable = vscode.commands.registerCommand('features-location.helloWorld', async () => {
         vscode.window.showInformationMessage("hello");
-        let uri = Uri.file('Samples/document4.txt');
+        // let uri = Uri.file('Samples/document4.txt');
 
 
-        let r = new Range(new Position(0, 2), new Position(0, 12))
+        // let r = new Range(new Position(0, 2), new Position(0, 12));
 
-        let success = await vscode.commands.executeCommand(
-            'vscode.executeInlineHintProvider', uri, r);
+        // let success = await vscode.commands.executeCommand(
+        //     'vscode.executeInlineHintProvider', uri, r);
 
     });
 
     let disposable2 = vscode.commands.registerCommand('features-location.explore', async (fileUri) => {
         vscode.window.showInformationMessage(fileUri);
         console.log(fileUri);
-    })
+    });
 
-
+    // context.subscriptions.push(disposableFeatures);
     context.subscriptions.push(disposable);
     context.subscriptions.push(disposable2);
+
+
 }
 
 // this method is called when your extension is deactivated
