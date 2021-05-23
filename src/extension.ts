b6e5853cb8bd9da7b79085fd0dc939a981d58204
edwinans <edyansari5@gmail.com>
2021-05-23 16:42:25 +0200

    [TreeView]: search buttong updated

--- src/extension.ts
+++ src/extension.ts
@@ -7,18 +7,18 @@ import { Range, Position } from 'vscode'
 export async function activate(context: vscode.ExtensionContext) {
 
 
-    let l: [String, Map<String, Range[]>][] = [];
+    // let l: [String, Map<String, Range[]>][] = [];
 
-    l.push(["feature1", new Map<String, Range[]>()]);
-    l.push(["feature2", new Map<String, Range[]>()]);
-    l[0][1].set("/home/edwin/Desktop/Cours/S2/PSTL/BankWebWithVariability/backend/src/Program.cs", [new Range(new Position(2, 3), new Position(4, 5))]);
-    l[1][1].set("/home/edwin/Desktop/Cours/S2/PSTL/BankWebWithVariability/backend/src/Controllers/AccountController.cs", [new Range(new Position(2, 3), new Position(4, 5)), new Range(new Position(1, 1), new Position(7, 7))]);
-    l[1][1].set("/home/edwin/Desktop/Cours/S2/PSTL/BankWebWithVariability/frontend/dist/scripts/data.js", [new Range(new Position(10, 10), new Position(11, 11)), new Range(new Position(1, 1), new Position(7, 7))]);
-
-
+    // l.push(["feature1", new Map<String, Range[]>()]);
+    // l.push(["feature2", new Map<String, Range[]>()]);
+    // l[0][1].set("/home/edwin/Desktop/Cours/S2/PSTL/BankWebWithVariability/backend/src/Program.cs", [new Range(new Position(2, 3), new Position(4, 5))]);
+    // l[1][1].set("/home/edwin/Desktop/Cours/S2/PSTL/BankWebWithVariability/backend/src/Controllers/AccountController.cs", [new Range(new Position(2, 3), new Position(4, 5)), new Range(new Position(1, 1), new Position(7, 7))]);
+    // l[1][1].set("/home/edwin/Desktop/Cours/S2/PSTL/BankWebWithVariability/frontend/dist/scripts/data.js", [new Range(new Position(10, 10), new Position(11, 11)), new Range(new Position(1, 1), new Position(7, 7))]);
 
 
-    new FeaturesLocator(context, undefined, l);
+
+
+    new FeaturesLocator(context);
 
     // let success = vscode.commands.executeCommand(
     //     'features-location.searchFeature', "main");
